8e827e83ca811e9ebdcdf27338875cddab0adda2
junlian <wujunlian@wanleyun.com>
2015-04-16 19:14:01 +0800

    tijai'

--- apps/mm/pages/square/route.js
+++ apps/mm/pages/square/route.js
@@ -6,9 +6,9 @@ var request = require('request');
 var thunkify = require('thunkify');
 
 module.exports = function(app, pageName) {
-  app.route('/' + pageName + '$').all(function*(next) {
-    this.result = {};
-    this.global.girlid = 0;
-    this.page = 'vod';
-  });
+	app.route('/' + pageName + '$').all(function*(next) {
+		this.result = {};
+		this.global.girlid = 0;
+		this.page = 'vod';
+	});
 }
