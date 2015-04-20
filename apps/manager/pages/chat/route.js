1cb382cdf651f7d04d989f63fe49bb2211f9ba97
Feng <yinfeng@wanleyun.com>
2015-04-20 16:04:54 +0800

    update

--- apps/manager/pages/chat/route.js
+++ apps/manager/pages/chat/route.js
@@ -12,30 +12,57 @@ var urlBase = 'http://' + 'account.' + global.WLY_DOMAIN
 
 module.exports = function(app) {
   app.route('/chat').all(function*(next) {
-    console.log('chat');
     this.result = {};
     this.template = 'chat/index';
   });
 
-  app.route('/api/userLogin').all(function*(next) {
-    console.log('/api/userLogin');
-    this.text = true;
-    // var result =
-    //   yield thunkify(request)({
-    //     url: urlBase + '/user/Login',
-    //     qs: this.request.query,
-    //     headers: {
-    //       'Cookie': this.headers.Cookie || ''
-    //     }
-    //   });
-    var result = {
-      a: 1
-    };
+  app.route('/api/userLogin$').all(function*(next) {
+    this.json = true;
+    var result =
+      yield thunkify(request)({
+        url: urlBase + '/user/Login',
+        qs: this.request.query,
+        proxy: 'http://127.0.0.1:8888',
+        qs: {
+          username: 'yinfeng',
+          password: 'db25f2fc14cd2d2b1e7af307241f548fb03c312a'
+        },
+        headers: {
+          'Cookie': this.headers.Cookie || ''
+        }
+      });
     if (result) {
-      var body = result;
-      if (body) {
-        this.set(result)
-      }
+      this.result = result;
+      console.log('result:');
+      console.log(result);
+    } else {
+      this.result = {
+        code: 'noLogin'
+      };
+    }
+  });
+
+  app.route('/api/push$').all(function*(next) {
+    this.json = true;
+    var result =
+      yield thunkify(request)({
+        url: 'http://ic.mm.wanleyun.com/demo/web/php/push.php',
+        qs: this.request.query,
+        proxy: 'http://127.0.0.1:8888',
+        qs: {
+          cname: 'girl_9',
+          content: 'asdfasdfasdf'
+        },
+        headers: {
+          'Cookie': this.headers.Cookie || ''
+        }
+      });
+    if (result) {
+      this.result = result;
+    } else {
+      this.result = {
+        code: 'noLogin'
+      };
     }
   });
 }
